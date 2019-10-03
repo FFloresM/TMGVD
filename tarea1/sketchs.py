e8bd1b2bb794ea7b568f7b56fbbf85e3bf5599a6
Francisco Flores <victoriafrancis2009@gmail.com>
2019-10-03 16:38:08 -0300

    countSketch countminCU

--- tarea1/sketchs.py
+++ tarea1/sketchs.py
@@ -1,37 +1,70 @@
 import random as rd
 import numpy as np
 import math as mt
+import mmh3
 
 class CountMin:
 	"""docstring for CountMin"""
-	a = []
-	b = []
-	p = []
 	def __init__(self, delta, epsilon):
 		self.d = int(mt.log(1/delta))
 		self.w = int(mt.exp(1)/epsilon)
 		self.sketch = np.zeros((self.d,self.w), dtype=int)
-		self.abp()
-
-	def abp(self, p=(2<<32)-1):
-		for i in range(self.d):
-			P = rd.randint(1,p)
-			a = rd.randint(1,P)
-			b = rd.randint(1,P)
-			self.a.append(a)
-			self.b.append(b)
-			self.p.append(P)
-
-	def hash(self, x, i):
-		return  ((self.a[i]*x+self.b[i])%self.p[i])%self.w
+		
+	def index(self, x, i):
+		"""value x, seed i"""
+		return mmh3.hash(x,i,signed=False)%self.w
 
 	def update(self, x):
 		for i in range(self.d):
-			self.sketch[i, self.hash(x,i)] +=1
+			self.sketch[i, self.index(x,i)] +=1
+
 
 	def query(self, x):
 		values = []
 		for i in range(self.d):
-			values.append(self.sketch[i, self.hash(x,i)])
+			values.append(self.sketch[i, self.index(x,i)])
 		return min(values)
 
+
+class CountSketch():
+	"""docstring for CountSketch"""
+	def __init__(self, delta, epsilon):
+		self.d = int(mt.log(4/delta))
+		self.w = int(1/(epsilon**2))
+		self.sketch = np.zeros((self.d,self.w), dtype=int)
+
+	def index(self, x, i):
+		"""value x, seed i"""
+		return mmh3.hash(x,i,signed=False)%self.w
+
+	def g(self, x, i):
+		return 1 if mmh3.hash(x,i) > 0 else -1
+
+	def update(self, x):
+		for i in range(self.d):
+			self.sketch[i, self.index(x,i)] += self.g(x,i) 
+
+	def query(self, x):
+		values = []
+		for i in range(self.d):
+			values.append(self.sketch[i, self.index(x,i)]*self.g(x,i))
+		return np.median(values)
+
+class CountMinCU(CountMin):
+	"""docstring for CountMinCU"""
+	def __init__(self, delta, epsilon):
+		CountMin.__init__(self, delta, epsilon)
+
+	def update(self, x):
+		minimos = []
+		for i in range(self.d):
+			for j in range(self.d):
+				minimos.append(self.sketch[i, self.index(x,i)])
+			_min = min(minimos)
+			if self.sketch[i, self.index(x,i)] == _min:
+				self.sketch[i, self.index(x,i)] +=1
+
+	def query(self, x):
+		return super().query(x)
+		
+		
--- tarea1/test.py
+++ tarea1/test.py
@@ -1,15 +1,27 @@
-from sketchs import CountMin
+from sketchs import CountMin, CountSketch, CountMinCU
 import random as rd
 
 delta = 0.01
 eps = 0.02
 cm = CountMin(delta, eps)
 print("d=", cm.d, "w=", cm.w)
-#print(cm.hashes)
-n = 10000000
-data = [rd.randint(0,100000) for x in range(n)]
+
+data = 'PARALELEPIPEDO'
+v = 'E'
 
 for i in data:
 	cm.update(i)
 
-print(cm.query(7))
+print(cm.query(v))
+
+cs = CountSketch(delta,eps)
+print("d=", cs.d, "w=", cs.w)
+for i in data:
+	cs.update(i)
+print(cm.query(v))
+
+cmcu = CountMinCU(delta,eps)
+print("d=", cmcu.d, "w=", cmcu.w)
+for i in data:
+	cmcu.update(i)
+print(cmcu.query(v))
