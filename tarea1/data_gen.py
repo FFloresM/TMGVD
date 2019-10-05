91efcb6d2713de6fee43324007f9ec535d30254d
Francisco Flores <victoriafrancis2009@gmail.com>
2019-10-05 02:31:48 -0300

    Add files via upload

--- tarea1/data_gen.py
+++ tarea1/data_gen.py
@@ -1,7 +1,16 @@
 import random
 import string
+import numpy as np
 
 def randomString(stringLength=10):
     """Generate a random string of fixed length """
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
+
+def zipf(a=2,n=100):
+	x = np.random.zipf(a,n)
+	x = [str(i) for i in x]
+	return x
+
+print(randomString(100))
+print(zipf())
